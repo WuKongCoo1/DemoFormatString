2efc467fc19dc0b643de34f22760e6cc70f567b3
Jacky Walker <wu_kong_coo1@163.com>
2019-09-09 16:46:48 +0800

    Initial Commit

---
+++ TestSprintF/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TestSprintF
+//
+//  Created by LINE Works on 2019/9/9.
+//  Copyright © 2019 LINE Works. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
